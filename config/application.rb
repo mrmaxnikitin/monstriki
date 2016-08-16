276f5f7d2bafb1943dddd54f486d1345bb3c8ec0
Max Nikitin <mr.maxnikitin@yandex.ru>
2016-08-16 22:06:04 +0300

    deploy

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,22 @@ Bundler.require(*Rails.groups)
 module Monstriki
   class Application < Rails::Application
 
+    config.generators do |g|
+        g.test_framework  nil
+        g.assets false
+        g.helper false
+    end
+      #config.browserify_rails.commandline_options = "-t reactify --extension=\".js.jsx\""
+      #LOCALIZATION
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Europe/Moscow'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
+      #EMAILING
+
     config.action_mailer.delivery_method = :mailgun
-     config.action_mailer.mailgun_settings = {
-            api_key: Rails.application.secrets.api_key,
-            domain: Rails.application.secrets.domain
+    config.action_mailer.mailgun_settings = {
+          api_key: Rails.application.secrets.api_key,
+          domain: Rails.application.secrets.domain
     }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
