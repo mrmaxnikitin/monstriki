6d9d95cb2db851979bc8c7d84166e99890f09dbb
Max Nikitin <mr.maxnikitin@yandex.ru>
2016-08-17 15:49:48 +0300

    deploy 1

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Monstriki
     #        domain: mail_config['domain']
    # }
 
-    #config.assets.initialize_on_precompile = false
-    
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
