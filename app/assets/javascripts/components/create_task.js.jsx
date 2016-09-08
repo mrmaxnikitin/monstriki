a42f17bc70a20cf2e3e61390a55db322e29801b0
Max Nikitin <mr.maxnikitin@yandex.ru>
2016-09-08 14:28:07 +0300

    исправил ошибку в jsx

--- app/assets/javascripts/components/create_task.js.jsx
+++ app/assets/javascripts/components/create_task.js.jsx
@@ -125,7 +125,7 @@ const CreateTask = React.createClass({
         </div>
         <input className={display_answer} type='text' ref='answer' placeholder='Ответ (последовательность из цифр в правильном порядке)' />
         <button onClick={this.reset}>Сбросить значания картинок.</button>
-        </br>
+        <br/>
         <button class='btn btn-our-green' onClick={this.createTask1}>Созидаем!</button>
       </div>
 
