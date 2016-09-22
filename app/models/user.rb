ffa8cd67b3ee969bb4e47b8448338c3c13dbdc14
Max Nikitin <mr.maxnikitin@yandex.ru>
2016-09-22 14:58:03 +0300

    валидация на почту

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   #validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: { message: 'Введите свою почту' },
-                    format: { message: 'Некорректная почта', with: VALID_EMAIL_REGEX, },
                     uniqueness: { message: 'Пользователь с такой почтой уже зарегистрирован' }
   validates :password,
     presence:     { message: 'Введите пароль' },
