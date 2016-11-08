09533e004420b41b190696b4309fdc81d80954a0
Max Nikitin <mr.maxnikitin@yandex.ru>
2016-11-09 00:52:00 +0300

    расширил места в рейтинге

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,7 @@
 class RatingsController < ApplicationController
 	def index
-		@riches = User.order(score: "DESC").all.limit(10)
+		@riches = User.order(score: "DESC").all.limit(20)
 		@new_users = User.all.order(created_at: "DESC").limit(10)
-		@user_tracks = Track.order(current_quest: "DESC").all.limit(10)
+		@user_tracks = Track.order(current_quest: "DESC").all.limit(20)
 	end
 end
