85724751b36017be8f1f7713c8bf05ebd1771aee
Max Nikitin <mr.maxnikitin@yandex.ru>
2016-11-11 20:55:30 +0300

    posts в admin

---
+++ app/admin/post.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Task do
+permit_params :title :img, :text
+actions :index, :show, :destroy, :edit, :update, :new, :create
+menu label: 'Посты'
+
+filter :id
+
+index do
+  id_column
+  column :title, label: "Заголовок"
+  column :text, label: "Текст"
+  column :img, label: "Изображение"
+  actions
+end
+
+show do
+  attributes_table do
+    row :id
+    row :title
+    row :text
+    row (:img) { |t| image_tag(t.img.to_s) }
+  end
+  active_admin_comments
+end
+
+end
