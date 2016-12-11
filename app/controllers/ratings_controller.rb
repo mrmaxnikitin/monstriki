f72dd3a3c92908f49bf628baf8f942babd6a4401
Max Nikitin <mr.maxnikitin@yandex.ru>
2016-12-11 17:55:42 +0300

    кол-во уровней в рейтиге

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,6 +2,6 @@ class RatingsController < ApplicationController
 	def index
 		@riches = User.order(score: "DESC").all.limit(20)
 		@new_users = User.all.order(created_at: "DESC").limit(10)
-		@user_tracks = Track.order(current_quest: "DESC").all.limit(20)
+		@user_tracks = Track.order(current_tour: "DESC").all.limit(20)
 	end
 end
